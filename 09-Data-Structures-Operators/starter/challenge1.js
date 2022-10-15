4d8b2f5ea54ea970ccfa9eeffca5ffdef7f698cf
larsen <larsen.cundric@longevize.com>
2022-10-15 10:52:23 +0200

    09-challenge1

---
+++ 09-Data-Structures-Operators/starter/challenge1.js
@@ -0,0 +1,55 @@
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski','Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [players1, players2] = game.players;
+const [[gk1, ...fieldPlayers1], [gk2, ...fieldPlayer21]] = game.players;
+const allPlayers = [...players1, ...players2];
+
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+const { team1, x: draw, team2 } = game.odds;
+
+function printGoals(...players) {
+  console.log(`Scored ${players.length} goals by ${players}`);
+}
+printGoals('Daviers', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+console.log(team1 > team2 && 'Team 1 is more likely to win');console.log(team1 < team2 && 'Team 2 is more likely to win');
